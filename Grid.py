803b1a929e3d2527287d5954e93e779b76099561
Leajian <leajian13@gmail.com>
2019-07-23 03:40:18 +0300

    WIP: Might be needed for game scenes

---
+++ Grid.py
@@ -0,0 +1,18 @@
+import os
+
+
+class Grid(object):
+    """WORK IN PROGRESS"""
+
+    def __init__(self, height, width):
+        super(Grid, self).__init__()
+        self.height = height
+        self.width = width
+        os.system('resize -s {} {}'.format(self.height, self.width))
+        os.system('clear')
+        self.tiles = [['+' for x in range(height)] for y in range(width)]
+
+    def printTiles(self):
+        for column in self.tiles:
+            for row in column:
+                print(row)
