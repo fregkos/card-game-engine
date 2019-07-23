edba1c107f4bf7b500c082d37fd2e140baddc4b5
Leajian <leajian13@gmail.com>
2019-07-23 03:38:01 +0300

    Changed card variable to lower case

--- Deck.py
+++ Deck.py
@@ -1,29 +1,30 @@
-from Card import Card
 import random
 
 
 class Deck(object):
 
-    def __init__(self, Cards=[]):
-
-        self.Cards = Cards
+    def __init__(self, cards=[]):
+        self.cards = cards
 
     def shuffleDeck(self):
-        random.shuffle(self.Cards)
+        random.shuffle(self.cards)
 
     def showAllCards(self, cardsPerLine):
 
-        for card in self.Cards:
+        for card in self.cards:
             card._constructASCIIform()
 
         # Using the first card to determine how long it is.
-        totalLines = len(self.Cards[0].printables)
+        totalLines = len(self.cards[0].printables)
 
-        for cards in range(0, len(self.Cards), cardsPerLine):
+        for cards in range(0, len(self.cards), cardsPerLine):
 
             for line in range(totalLines):
 
-                for card in self.Cards[cards:cards + cardsPerLine]:
+                for card in self.cards[cards:cards + cardsPerLine]:
                     print(card.printables[line], end='')
 
                 print()
+
+    def getCards(self):
+        return self.cards
